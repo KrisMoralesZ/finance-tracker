61faf24dcc58ecc9042b46a3d71ae0ac38955b0f
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-02-11 19:43:29 -0700

    create Stock Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_05_025222) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_12_023923) do
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticket"
+    t.string "name"
+    t.decimal "last_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
