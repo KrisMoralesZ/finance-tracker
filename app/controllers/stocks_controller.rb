9ae700eb364ddf4d516acbb37b8c7d23f5480a7a
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-03-20 12:25:16 -0700

    delete unused actions

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -11,53 +11,6 @@ class StocksController < ApplicationController
   def show
   end
 
-  # GET /stocks/new
-  def new
-    @stock = Stock.new
-  end
-
-  # GET /stocks/1/edit
-  def edit
-  end
-
-  # POST /stocks or /stocks.json
-  def create
-    @stock = Stock.new(stock_params)
-
-    respond_to do |format|
-      if @stock.save
-        format.html { redirect_to @stock, notice: "Stock was successfully created." }
-        format.json { render :show, status: :created, location: @stock }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @stock.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /stocks/1 or /stocks/1.json
-  def update
-    respond_to do |format|
-      if @stock.update(stock_params)
-        format.html { redirect_to @stock, notice: "Stock was successfully updated." }
-        format.json { render :show, status: :ok, location: @stock }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @stock.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /stocks/1 or /stocks/1.json
-  def destroy
-    @stock.destroy
-
-    respond_to do |format|
-      format.html { redirect_to stocks_path, status: :see_other, notice: "Stock was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   def stock_data
     time_series = GetTimeSeriesService.get_time_series(@stock.ticker)
     @chart_data = time_series.map { |data| [Time.at(data["t"]).to_date, data["c"]]} if time_series
@@ -75,8 +28,6 @@ class StocksController < ApplicationController
       if @stock
         price_response = GetPriceService.get_price(@stock.ticker)
         @stock_price = price_response["p"] ? price_response["p"] : "Price not found"
-
-
       end
       unless @stock
         render file: "#{Rails.root}/public/404.html", status: :not_found
