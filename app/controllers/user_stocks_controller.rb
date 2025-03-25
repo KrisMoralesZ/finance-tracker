2c2374c8df5059b70ce4130baeed8d004668f91a
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-03-25 12:28:47 -0700

    fix stock price and add delete button

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -6,4 +6,16 @@ class UserStocksController < ApplicationController
 
     redirect_to my_stocks_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = current_user.user_stocks.find_by(stock: stock)
+
+    if user_stock
+      user_stock.destroy
+      redirect_to my_stocks_path, notice: "Stock Deleted"
+    else
+      redirect_to my_stocks_path, notice: "Stock not found"
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def my_stocks
     @tracked_stocks = current_user.stocks
+
+    @stock_prices ={}
+    @tracked_stocks.each do |user_stock|
+      price_response = GetPriceService.get_price(user_stock.ticker)
+      @stock_prices[user_stock.ticker] = price_response["p"] || "N/A"
+    end
   end
 end
